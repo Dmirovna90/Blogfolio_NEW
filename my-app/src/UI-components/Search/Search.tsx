baf40052ead5f817e04442661a2d6516045e5eee
Darya Zheltok <dmirovna@yandex.by>
2025-01-07 16:00:42 +0300

    HW 48, 50, 51 in progress

--- my-app/src/UI-components/Search/Search.tsx
+++ my-app/src/UI-components/Search/Search.tsx
@@ -2,17 +2,43 @@ import style from './Search.module.scss'
 import { searchActive } from '../../store/searchSlice';
 import {ReactComponent as SearchImg} from '../../assets/search.svg';
 import SearchInput from '../SearchInput/SearchInput';
-import Input from '../../Components/Input/Input';
 import { useDispatch, useSelector } from 'react-redux';
-import {ReactComponent as Cancel} from '../../assets/cancelIcon.svg';
+import { useEffect } from 'react';
+import { fetchPosts, setPage, setSearchQuery } from '../../store/postsSlice';
 
 const Search = () => {
-    const {isActive} = useSelector((state) => state.search);
+    const {
+        searchQuery,
+      } = useSelector((state) => state.posts);
+    useEffect(() => {
+        dispatch(
+          fetchPosts({
+            searchQuery: searchQuery,
+          })
+        );
+      }, []);
     const dispatch = useDispatch();
-    return( 
-        <button className = {style.searchButton} onClick={() => dispatch(searchActive())}>
+    const handlerSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(
+          fetchPosts({
+            searchQuery: searchQuery,
+          })
+        );
+        dispatch(setPage(1));
+      };
+      const handlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        dispatch(setSearchQuery(value));
+      };
+    return(
+        <form onSubmit = {handlerSubmit} className = {style.searchWrap}>
+            <SearchInput type = {'text'} placeholder = {'Search...'} value = {searchQuery} onChange={handlerInput}/>
+            <button type = 'submit' className = {style.searchButton} onClick={() => dispatch(searchActive())}>
                 <SearchImg/>
-        </button>
+            </button>
+        </form>
+
     )
 }
 export default Search
--- my-app/src/UI-components/SearchInput/SearchInput.tsx
+++ my-app/src/UI-components/SearchInput/SearchInput.tsx
@@ -1,19 +1,31 @@
 import { useDispatch, useSelector } from 'react-redux';
 import {ReactComponent as Cancel} from '../../assets/cancelIcon.svg';
-import { toggleActive } from '../../store/activeSlice';
-import Search from '../Search/Search';
 import { searchActive } from '../../store/searchSlice';
 import style from './SearchInput.module.scss';
-const SearchInput = () => {
-    // const dispatch = useDispatch(); 
-    // const {isActive} = useSelector((state) => state.search);
-    // return (
-    // <div className={!isActive ? style.search : `${style.search} ${style.active}`}>
-    //     <input></input>
-    //     <button onClick={() => dispatch(searchActive())}>
-    //     <Cancel />
-    //     </button>
-    // </div>
-    // )
+interface IProps {
+    placeholder?: string,
+    type: string,
+    name?: string,
+    value?: string,
+    onChange?: any,
+}
+const SearchInput = ({type = 'text', placeholder = "Search...", name, value, onChange}: IProps) => {
+    const dispatch = useDispatch();
+    const {isActive} = useSelector((state) => state.search);
+    return (
+    <div className={!isActive ? style.search : `${style.search} ${style.active}`}>
+        <input
+            className = {style.inputField}
+            type = {type}
+            placeholder = {placeholder}
+            name = {name}
+            value = {value}
+            onChange = {onChange}
+        ></input>
+        <button className = {style.cancelButton} onClick={() => dispatch(searchActive())}>
+            <Cancel className = {style.cancel}/>
+        </button>
+    </div>
+    )
 }
 export default SearchInput
